96db0d972d3efeb0248326fa7b386cb8bb22dd89
Adam R <121298839+abtzpro@users.noreply.github.com>
2023-06-13 00:43:22 -0500

    Update AITrainer.py
    This version of the python AITrainer.py script will fine tune a pre-trained GPT2 AI model on IBM's project CodeNet if it is fed as the training dataset thus fine tuning the AI on the art of generating working programming code. (such is the current working theory prior to thorough testing)

--- AITrainer.py
+++ AITrainer.py
@@ -1,31 +1,45 @@
 import os
 import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, LineByLineTextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+from pathlib import Path
 
 # Define the model directory
 MODEL_DIR = "trained_model"
 
-# Fine-tune the model
-def fine_tune_model():
-    num_epochs = 3
-    batch_size = 4
-    learning_rate = 5e-5
-    training_file_path = "training_corpus.txt"
-    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-    
-    dataset = LineByLineTextDataset(
+# Load the dataset
+def load_dataset(train_path, test_path, tokenizer):
+    train_dataset = TextDataset(
         tokenizer=tokenizer,
-        file_path=training_file_path,
+        file_path=train_path,
         block_size=128,
     )
+
+    test_dataset = TextDataset(
+        tokenizer=tokenizer,
+        file_path=test_path,
+        block_size=128,
+    )
+
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer,
         mlm=False,
     )
 
-    train_dataset = dataset
-    eval_dataset = dataset
+    return train_dataset, test_dataset, data_collator
 
+# Fine-tune the model
+def fine_tune_model():
+    num_epochs = 250
+    batch_size = 4
+    learning_rate = 5e-5
+    
+    train_path = Path('path/to/your/train/dataset')  # Adjust these paths
+    test_path = Path('path/to/your/test/dataset')
+
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+    train_dataset, test_dataset, data_collator = load_dataset(train_path, test_path, tokenizer)
+    
     model = GPT2LMHeadModel.from_pretrained("gpt2")
     training_args = TrainingArguments(
         output_dir=MODEL_DIR,
@@ -42,7 +56,7 @@ def fine_tune_model():
         args=training_args,
         train_dataset=train_dataset,
         data_collator=data_collator,
-        eval_dataset=eval_dataset,
+        eval_dataset=test_dataset,
         prediction_loss_only=True,
     )
     trainer.train()
@@ -50,6 +64,8 @@ def fine_tune_model():
     # Save tokenizer and model
     tokenizer.save_pretrained(MODEL_DIR)
     model.save_pretrained(MODEL_DIR)
+
+    return tokenizer, trainer
 
 # Generate text using the trained model
 def generate_text(prompt, model_path=MODEL_DIR, max_length=200, num_return_sequences=1):
@@ -63,42 +79,16 @@ def generate_text(prompt, model_path=MODEL_DIR, max_length=200, num_return_seque
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     return generated_text
 
-# Generate corpus using the trained model
-def generate_corpus(prompt, model_path=MODEL_DIR, max_length=1000, num_return_sequences=1, num_lines=100):
-    tokenizer = GPT2Tokenizer.from_pretrained(model_path)
-    model = GPT2LMHeadModel.from_pretrained(model_path)
-
-    corpus = []
-    for _ in range(num_lines):
-        input_ids = tokenizer.encode(prompt, return_tensors="pt")
-        with torch.no_grad():
-            output = model.generate(input_ids, max_length=max_length, num_return_sequences=num_return_sequences, do_sample=True, top_k=50)
-
-        generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-        corpus.append(generated_text)
-
-    return corpus
-
 if not os.path.exists(MODEL_DIR):
     os.makedirs(MODEL_DIR)
-    fine_tune_model()
+    tokenizer, trainer = fine_tune_model()
 
-generated_text = generate_text(prompt="Hello, how are you?")
+generated_text = generate_text(prompt="function")
 print(generated_text)
 
-training_corpus = generate_corpus(prompt="Cybersecurity is", num_lines=1000)
-with open('training_corpus.txt', 'w') as f:
-    for line in training_corpus:
-        f.write("%s\n" % line)
-
-test_corpus = generate_corpus(prompt="Programming languages", num_lines=100)
-with open('test_corpus.txt', 'w') as f:
-    for line in test_corpus:
-	        f.write("%s\n" % line)
-
 # Evaluate the model
-test_file_path = "test_corpus.txt"
-test_dataset = LineByLineTextDataset(
+test_file_path = "path/to/your/test/dataset"
+test_dataset = TextDataset(
     tokenizer=tokenizer,
     file_path=test_file_path,
     block_size=128,
@@ -107,15 +97,5 @@ test_loss = trainer.evaluate(test_dataset=test_dataset)
 print(f"Test loss: {test_loss}")
 
 # Example usage
-generated_text = generate_text(prompt="Hello, how are you?")
+generated_text = generate_text(prompt="function")
 print(generated_text)
-
-training_corpus = generate_corpus(prompt="Cybersecurity is", num_lines=1000)
-with open('training_corpus.txt', 'w') as f:
-    for line in training_corpus:
-        f.write("%s\n" % line)
-
-test_corpus = generate_corpus(prompt="Programming languages", num_lines=100)
-with open('test_corpus.txt', 'w') as f:
-    for line in test_corpus:
-        f.write("%s\n" % line)
